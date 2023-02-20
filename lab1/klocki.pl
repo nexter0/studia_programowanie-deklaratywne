f080409506faaf388a06976de334b6e2af89db12
Nikodem <97964965+nexter0@users.noreply.github.com>
2023-02-20 13:57:06 +0100

    Create klocki.pl

---
+++ lab1/klocki.pl
@@ -0,0 +1,13 @@
+% ===================================
+
+% na (X, Y)
+% opis: spełniony, gdy klocek X leży bezpośrednio na klocku Y
+% -----------------------------na/2
+ na(c,a).
+ na(c,b).
+ na(d,c).
+% -----------------------------na/2
+% 3 klauzule
+% 1 definicja relacji
+% relacja na/2, ktora sklada sie z:
+% 3 klauzuli, ktore sa faktami
