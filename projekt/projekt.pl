fd42483d58cf958661f8a8484de685c292e5a6e6
Nikodem <97964965+nexter0@users.noreply.github.com>
2023-05-22 15:10:23 +0200

    Update projekt.pl

--- projekt/projekt.pl
+++ projekt/projekt.pl
@@ -1,5 +1,4 @@
 % sejfy i kasy pancerne
-% Hartmann Tresore
 % sejf/6
 % sejf(Nazwa, Typ, Rozmiar, LimitWartosci, Cena, Zamek)
 sejf('Sejf domowy D 109-08', 
@@ -8,5 +7,25 @@ sejf('Sejf antywłamaniowy F 103-01 Blanco',
      'Biurowy', 'Mały', 20000, 1200, 'Klucz').
 sejf('Sejf biurowy HTIV 415-21',
      'Biurowy', 'Mały', 150000, 1200, 'Szyfr').
-sejf('Sejf na broń WT 270-01',
+sejf('Sejf na broń długą WT 270-01',
      'Na broń', 'Duży', 20000, 3500, 'Szyfr').
+sejf('Sejf na broń krótką WS 145-01',
+     'Na broń', 'Mały', 2500, 1000, 'Klucz').
+sejf('Szafa pancerna EP 233-2A',
+     'Szafa', 'Duży', 50000, 5400, 'Klucz').
+sejf('Szafa pancerna EPX 238-SX',
+     'Szafa', 'Duży', 100000, 8300, 'Szyfr').
+sejf('Szafa pancerna EXT 22A V100',
+     'Szafa', 'Duży', 75000, 11000, 'Elektroniczny').
+sejf('Szafa pancerna EXT 22C-B V120',
+     'Szafa', 'Duży', 75000, 26450, 'Biometria').
+sejf('Sejf domowy D 22-11',
+     'Domowy', 'Średni', 20000, 1800, 'Szyfr').
+sejf('Sejf antywłamaniowy MAG3-223EX',
+     'Biurowy', 'Średni', 50000, 6500, 'Szyfr').
+sejf('Sejf na laptopa HS 420-02 V110',
+     'Hotelowy', 'Mały', 2000, 950, 'Elektroniczny').
+sejf('Sejf na dokumenty HS 421-DC V100',
+     'Hotelowy', 'Mały', 5000, 1500, 'Elektroniczny').
+sejf('Sejf biometryczny HS 420-02 V110',
+     'Hotelowy', 'Mały', 5000, 3100, 'Biometria').
