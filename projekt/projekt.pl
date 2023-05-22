1b2edd9c48da16389be01145b9f4284f2d5ff8dc
Nikodem <97964965+nexter0@users.noreply.github.com>
2023-05-22 14:55:40 +0200

    Create projekt.pl

---
+++ projekt/projekt.pl
@@ -0,0 +1,12 @@
+% sejfy i kasy pancerne
+% Hartmann Tresore
+% sejf/6
+% sejf(Nazwa, Typ, Rozmiar, LimitWartosci, Cena, Zamek)
+sejf('Sejf domowy D 109-08', 
+     'Domowy', 'Mały', 20000, 700, 'Klucz').
+sejf('Sejf antywłamaniowy F 103-01 Blanco',
+     'Biurowy', 'Mały', 20000, 1200, 'Klucz').
+sejf('Sejf biurowy HTIV 415-21',
+     'Biurowy', 'Mały', 150000, 1200, 'Szyfr').
+sejf('Sejf na broń WT 270-01',
+     'Na broń', 'Duży', 20000, 3500, 'Szyfr').
